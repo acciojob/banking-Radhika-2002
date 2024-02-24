7602bfb881eda319c00d4c324bfb7eca6a5078e1
Radhika-2002 <2019radhika.katiyara@ves.ac.in>
2024-02-25 00:22:51 +0530

    Update SavingsAccount.java

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -1,28 +1,33 @@
 package com.driver;
 
-public class SavingsAccount extends BankAccount{
+public class SavingsAccount extends BankAccount {
     double rate;
     double maxWithdrawalLimit;
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
-        // minimum balance is 0 by default
-
+        super(name, balance, 0); // minimum balance is 0 by default
+        this.rate = rate;
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
     }
+
     public void withdraw(double amount) throws Exception {
-        // Might throw the following errors:
-        // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
-        // 2. "Insufficient Balance" : If the amount exceeds balance
-
+        if (amount > maxWithdrawalLimit) {
+            throw new Exception("Maximum Withdraw Limit Exceed");
+        } else if (amount > balance) {
+            throw new Exception("Insufficient Balance");
+        }
+        balance -= amount;
     }
 
-    public double getSimpleInterest(int years){
-        // Return the final amount considering that bank gives simple interest on current amount
-
+    public double getSimpleInterest(int years) {
+        // Logic to calculate simple interest
+        // Not implemented in this example
+        return 0;
     }
 
-    public double getCompoundInterest(int times, int years){
-        // Return the final amount considering that bank gives compound interest on current amount given times per year
-
+    public double getCompoundInterest(int times, int years) {
+        // Logic to calculate compound interest
+        // Not implemented in this example
+        return 0;
     }
-
 }
